0bc2e7c3708ee9a8eb5283f721efb8e02a158815
ls <ryanadrig@gmail.com>
2022-02-26 03:34:41 -0800

    bu

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,10 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:sensors/sensors.dart';
+
 import 'snake.dart';
 
-var gss =  Size(300,500);
-String input_setting = "butt";
-
-TextStyle ui_but_ts = TextStyle(fontSize: gss.width*.09);
-TextStyle snek_title_style = TextStyle(fontSize: gss.height*.05,
-fontWeight: FontWeight.w700,
-  fontFamily: 'MontserratSubrayada'
-);
-
 void main() {
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    // DeviceOrientation.portraitDown,
-  ]);
   runApp(MyApp());
 }
 
@@ -29,26 +16,7 @@ class SnakeStart extends StatefulWidget {
 class _SnakeStartState extends State<SnakeStart> {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      // DeviceOrientation.portraitDown,
-    ]);
-    gss = MediaQuery.of(context).size;
-
-    return Scaffold(body:
-Container(height:gss.height,
-    child:ListView(children:[
-      Container(
-        height: gss.height*.1,
-      ),
-    Container(height: gss.height*.28,
-    child: Center(child: Text("Snek_SI",
-    style: snek_title_style,
-    ),),
-    ),
-    Container(
-        width: gss.width*.66,
-        child:
+    return Scaffold(body: Center(child:
       GestureDetector(onTap: (){
       Navigator.push(
         context,
@@ -56,28 +24,8 @@ Container(height:gss.height,
             Snek_si_play()
       )
       );},
-        child:
-            ClipRRect(
-                borderRadius: BorderRadius.circular(gss.width*.08),
-                child:
-            Container(
-              color: Colors.white,
-              width: gss.width*.66,
-                height: gss.width *.14,
-                padding: EdgeInsets.all(gss.width*.02),
-                child:
-    ClipRRect(
-    borderRadius: BorderRadius.circular(gss.width*.08),
-    child:
-        Container(
-          width: gss.width*.64,
-          color: Colors.blueGrey[900],
-          child:Center(child: Text("start", style: ui_but_ts,),),))))
-      )),
-
-
-    ]
-    )));
+        child: Center(child: Text("start"),),
+      )));
 }}
 
 
@@ -102,8 +50,8 @@ class Snek_si_play extends StatefulWidget {
 }
 
 class _Snek_si_playState extends State<Snek_si_play> {
-  static const int _snakeRows = 40;
-  static const int _snakeColumns = 40;
+  static const int _snakeRows = 20;
+  static const int _snakeColumns = 20;
   static const double _snakeCellSize = 10.0;
 
   List<double> _accelerometerValues;
@@ -112,13 +60,8 @@ class _Snek_si_playState extends State<Snek_si_play> {
   List<StreamSubscription<dynamic>> _streamSubscriptions =
   <StreamSubscription<dynamic>>[];
 
-
-bool left_press;
-bool right_press;
-
   @override
   Widget build(BuildContext context) {
-
     final List<String> accelerometer =
     _accelerometerValues?.map((double v) => v.toStringAsFixed(1))?.toList();
     final List<String> gyroscope =
@@ -126,13 +69,6 @@ bool right_press;
     final List<String> userAccelerometer = _userAccelerometerValues
         ?.map((double v) => v.toStringAsFixed(1))
         ?.toList();
-
-    reset_press(){
-      setState(() {
-        left_press = false;
-            right_press = false;
-      });
-    }
 
     return
       WillPopScope(
@@ -164,74 +100,37 @@ bool right_press;
                   rows: _snakeRows,
                   columns: _snakeColumns,
                   cellSize: _snakeCellSize,
-                  input_setting: input_setting,
-                  right_press: right_press,
-                  left_press: left_press,
-                  reset_press: reset_press
                 ),
               ),
             ),
           ),
-          Container(height: gss.height*.06,),
-          Container(
-            // height: gss.height*.2,
+          Padding(
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                GestureDetector(
-                    onTap: (){
-                      print("Left Press");
-                      setState(() {
-    left_press = true;
-    right_press = false;
-                      });
-                    },
-                    child:
-                ClipRRect(
-                    borderRadius: BorderRadius.circular(gss.width*.08),
-                    child:
-                    Container(
-                        color: Colors.white,
-                        width: gss.width*.26,
-                        height: gss.width * .20,
-                        padding: EdgeInsets.all(gss.width*.02),
-                        child:
-                        ClipRRect(
-                            borderRadius: BorderRadius.circular(gss.width*.08),
-                            child:
-                            Container(
-                              width: gss.width*.64,
-                              color: Colors.blueGrey[900],
-                              child:Center(child: Icon(Icons.chevron_left),),))))),
-GestureDetector(
-    onTap: (){
-      print("Right press");
-      setState(() {
-right_press = true;
-left_press = false;
-      });
-    },
-    child:
-                ClipRRect(
-                    borderRadius: BorderRadius.circular(gss.width*.08),
-                    child:
-                    Container(
-                        color: Colors.white,
-                        width: gss.width*.26,
-                        height: gss.width * .20,
-                        padding: EdgeInsets.all(gss.width*.02),
-                        child:
-                        ClipRRect(
-                            borderRadius: BorderRadius.circular(gss.width*.08),
-                            child:
-                            Container(
-                              width: gss.width*.64,
-                              color: Colors.blueGrey[900],
-                              child:Center(child: Icon(Icons.chevron_right),),))))),
-
-
-              ],),
-          )
+                Text('Accelerometer: $accelerometer'),
+              ],
+            ),
+            padding: const EdgeInsets.all(16.0),
+          ),
+          Padding(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('UserAccelerometer: $userAccelerometer'),
+              ],
+            ),
+            padding: const EdgeInsets.all(16.0),
+          ),
+          Padding(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Gyroscope: $gyroscope'),
+              ],
+            ),
+            padding: const EdgeInsets.all(16.0),
+          ),
         ],
       ),
     ));
